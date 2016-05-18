f02cc70926a810d0b0b736ddf9d97e383b5edc1d
lynn-ita <ealie@list.ru>
2016-05-19 02:27:17 +0300

    Финальная проверка №2

--- js/script.js
+++ js/script.js
@@ -14,18 +14,3 @@ window.addEventListener("keydown", function(event) {
         }
     }
 });
-
-var map;
-function initialize() {
-    var mapOptions = {
-        center: new google.maps.LatLng(34.86917, -111.76168),
-        zoom: 8,
-};
-map = new google.maps.Map(document.getElementById("map"), Options);
-}
-google.maps.event.addDomListener(window, 'load', initialize);
-
-var marker = new google.maps.Marker({
- position: new google.maps.LatLng(34.86917, -111.76168),
- map: map
-});
